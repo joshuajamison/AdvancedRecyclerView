e1c869ddd3cc3cae98551eff41805d8c764c3a2e
Joshua Jamison <joshua.jamison@gmail.com>
2016-02-17 11:13:39 +0900

    cleans up complex decoration

--- app/src/main/java/com/codeundone/advancedrecyclerview/decoration/complex/ComplexDecoration.java
+++ app/src/main/java/com/codeundone/advancedrecyclerview/decoration/complex/ComplexDecoration.java
@@ -6,6 +6,7 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.Typeface;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextPaint;
 import android.text.TextUtils;
@@ -15,33 +16,33 @@ import com.codeundone.advancedrecyclerview.R;
 
 public class ComplexDecoration extends RecyclerView.ItemDecoration {
 
-    private final Callback mCallback;
-    private final TextPaint mTextPaint;
-    private final Paint mPaint;
-    private final int mLeftGap;
-    private final int mTopGap;
-    private final int mRightGap;
-    private Paint.FontMetrics mFontMetrics;
+    private final Callback callback;
+    private final TextPaint textPaint;
+    private final Paint paint;
+    private final int leftGap;
+    private final int topGap;
+    private final int rightGap;
+    private Paint.FontMetrics fontMetrics;
 
     public ComplexDecoration(Context context, Callback callback) {
         super();
         final Resources res = context.getResources();
-        mTextPaint = new TextPaint();
-        mPaint = new Paint();
-        mCallback = callback;
+        textPaint = new TextPaint();
+        paint = new Paint();
+        this.callback = callback;
 
-        mTextPaint.setTypeface(Typeface.DEFAULT_BOLD);
-        mTextPaint.setAntiAlias(true);
-        mTextPaint.setTextSize(80);
-        mTextPaint.setColor(res.getColor(R.color.colorPrimaryDark));
-        mTextPaint.getFontMetrics(mFontMetrics);
-        mTextPaint.setTextAlign(Paint.Align.CENTER);
-        mPaint.setColor(res.getColor(R.color.colorAccent));
-        mFontMetrics = new Paint.FontMetrics();
+        textPaint.setTypeface(Typeface.DEFAULT_BOLD);
+        textPaint.setAntiAlias(true);
+        textPaint.setTextSize(80);
+        textPaint.setColor(res.getColor(R.color.colorPrimaryDark));
+        textPaint.getFontMetrics(fontMetrics);
+        textPaint.setTextAlign(Paint.Align.LEFT);
+        paint.setColor(res.getColor(R.color.colorAccent));
+        fontMetrics = new Paint.FontMetrics();
 
-        mLeftGap = res.getDimensionPixelSize(R.dimen.indent_pad_left);
-        mRightGap = res.getDimensionPixelSize(R.dimen.indent_pad_right);
-        mTopGap = res.getDimensionPixelSize(R.dimen.indent_pad_top);
+        leftGap = res.getDimensionPixelSize(R.dimen.indent_pad_left);
+        rightGap = res.getDimensionPixelSize(R.dimen.indent_pad_right);
+        topGap = res.getDimensionPixelSize(R.dimen.indent_pad_top);
     }
 
     /**
@@ -56,14 +57,14 @@ public class ComplexDecoration extends RecyclerView.ItemDecoration {
         super.getItemOffsets(outRect, view, parent, state);
 
         int pos = parent.getChildAdapterPosition(view);
-        long groupId = mCallback.getGroupId(pos);
+        long groupId = callback.getGroupId(pos);
         if (groupId < 0) return;
 
-        outRect.left = mLeftGap;
-        outRect.right = mRightGap;
+        outRect.left = leftGap;
+        outRect.right = rightGap;
 
         if (pos == 0 || isFirstInGroup(pos)) {
-            outRect.top = mTopGap;
+            outRect.top = topGap;
         } else {
             outRect.top = 0;
         }
@@ -75,46 +76,36 @@ public class ComplexDecoration extends RecyclerView.ItemDecoration {
 
         final int totalItemCount = state.getItemCount();
         final int childCount = parent.getChildCount();
-        if (childCount == 0) return;
+        final float left = parent.getPaddingLeft();
+        final float lineHeight = textPaint.getTextSize() + fontMetrics.descent;
 
-        float left = parent.getPaddingLeft(), textY, viewBottom;
-        float textHeight = mTextPaint.getTextSize();
-        float textX = left + mLeftGap / 2;
-        float lineHeight = textHeight + mFontMetrics.descent;
-        View view;
-        int position;
-
-        String textLine;
-        long groupId, prevGroupId = -1;
+        long prevGroupId, groupId = -1;
         for (int i = 0; i < childCount; i++) {
-            view = parent.getChildAt(i);
-            position = parent.getChildAdapterPosition(view);
-            viewBottom = view.getBottom() + view.getPaddingBottom();
+            final View view = parent.getChildAt(i);
+            final int position = parent.getChildAdapterPosition(view);
 
-            // Ignore items without a group
-            groupId = mCallback.getGroupId(position);
+            // Ignore items without a group and items that are in same group as previous item
+            prevGroupId = groupId;
+            groupId = callback.getGroupId(position);
             if (groupId < 0 || groupId == prevGroupId) continue;
 
-            // Ignore empty textline
-            textLine = mCallback.getGroupFirstLine(position);
+            // Ignore empty text line
+            final String textLine = callback.getGroupFirstLine(position).toUpperCase();
             if (TextUtils.isEmpty(textLine)) continue;
 
-            // Keep the text on screen
-            textY = Math.max(mTopGap, view.getTop() + textHeight);
-
-            // Keep text above viewBottom of group if this is the last item in it's group
+            // Find appropriate y position for text; on screen unless pushed off by bottom of group
+            final int viewBottom = view.getBottom() + view.getPaddingBottom();
+            float textY = Math.max(topGap, view.getTop() + view.getPaddingTop());
             if (position + 1 < totalItemCount) {
-                long nextGroupId = mCallback.getGroupId(position + 1);
+                long nextGroupId = callback.getGroupId(position + 1);
                 if (nextGroupId != groupId && viewBottom < textY + lineHeight) {
+                    // Next item is different group, align Y with bottom of current group
                     textY = viewBottom - lineHeight;
                 }
             }
 
             // draw text
-            textLine = mCallback.getGroupFirstLine(position);
-            c.drawText(textLine.toUpperCase(), textX, textY, mTextPaint);
-
-            prevGroupId = groupId;
+            c.drawText(textLine, left, textY, textPaint);
         }
     }
 
@@ -122,8 +113,8 @@ public class ComplexDecoration extends RecyclerView.ItemDecoration {
         if (position == 0) {
             return true;
         } else {
-            long prevGroupId = mCallback.getGroupId(position - 1);
-            long groupId = mCallback.getGroupId(position);
+            long prevGroupId = callback.getGroupId(position - 1);
+            long groupId = callback.getGroupId(position);
             return prevGroupId != groupId;
         }
     }
@@ -131,6 +122,6 @@ public class ComplexDecoration extends RecyclerView.ItemDecoration {
     public interface Callback {
         long getGroupId(int position);
 
-        String getGroupFirstLine(int position);
+        @NonNull String getGroupFirstLine(int position);
     }
 }
